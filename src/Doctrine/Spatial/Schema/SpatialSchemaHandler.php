7e17eda854efd4151b99b22a376b7fa03361ef52
jsor <jan@sorgalla.com>
2011-12-07 11:46:17 +0100

    Use own doctrine2 fork

--- src/Doctrine/Spatial/Schema/SpatialSchemaHandler.php
+++ src/Doctrine/Spatial/Schema/SpatialSchemaHandler.php
@@ -18,6 +18,7 @@ use Doctrine\DBAL\Schema\Column;
 use Doctrine\DBAL\Schema\ColumnDiff;
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Schema\TableDiff;
+use Doctrine\DBAL\Types\Type;
 
 /**
  * @author  Jan Sorgalla <jsorgalla@googlemail.com>
@@ -29,13 +30,13 @@ class SpatialSchemaHandler implements CustomSchemaHandler
     {
         switch ($conn->getDatabasePlatform()->getName()) {
             case 'postgresql':
-                return $this->getPortablePostgresTableColumnDefinition($table, $database, $tableColumn, $conn);
+                return $this->getPortablePostgresqlTableColumnDefinition($table, $database, $tableColumn, $conn);
             default:
                 return false;
         }
     }
 
-    protected function getPortablePostgresTableColumnDefinition($table, $database, $tableColumn, Connection $conn)
+    protected function getPortablePostgresqlTableColumnDefinition($table, $database, $tableColumn, Connection $conn)
     {
         $tableColumn = array_change_key_case($tableColumn, CASE_LOWER);
 
@@ -65,118 +66,253 @@ class SpatialSchemaHandler implements CustomSchemaHandler
                 )
             );
 
-            return new SpatialColumn($tableColumn['field'], \Doctrine\DBAL\Types\Type::getType($type), $options);
+            return new SpatialColumn($tableColumn['field'], Type::getType($type), $options);
         }
 
         return false;
     }
-    
+
+    public function getColumnFromMetadata($class, array $mapping, $table, AbstractPlatform $platform)
+    {
+        switch ($platform->getName()) {
+            case 'postgresql':
+                if ($this->isGeometryColumn($mapping['type'])) {
+                    $columnName = $class->getQuotedColumnName($mapping['fieldName'], $platform);
+
+                    $platformOptions = array();
+                    $platformOptions['version'] = $class->isVersioned && $class->versionField == $mapping['fieldName'] ? true : false;
+
+                    $options = array(
+                        'length'          => null,
+                        'notnull'         => isset($mapping['nullable']) ? !$mapping['nullable'] : true,
+                        'default'         => isset($mapping['default']) ? $mapping['default'] : null,
+                        'primary'         => false,
+                        'precision'       => null,
+                        'scale'           => null,
+                        'fixed'           => null,
+                        'unsigned'        => false,
+                        'autoincrement'   => false,
+                        'comment'         => null,
+                        'platformOptions' => $platformOptions,
+                        'spatialOptions'  => array(
+                            //'srid'            => (int) $row['srid'],
+                            //'coord_dimension' => (int) $row['coord_dimension'],
+                        )
+                    );
+
+                    return new SpatialColumn($columnName, Type::getType($mapping['type']), $options);
+                }
+                break;
+        }
+
+        return false;
+    }
+
     public function getColumnDeclarationSQL($name, array $field, AbstractPlatform $platform)
     {
         switch ($platform->getName()) {
             case 'postgresql':
-                switch (strtolower($field['type']->getName())) {
-                    case 'point':
-                    case 'linestring':
-                    case 'polygon':
-                    case 'multipoint':
-                    case 'multilinestring':
-                    case 'multipolygon':
-                    case 'geometrycollection':
-                        return null; // Skip
+                if ($this->isGeometryColumn($field['type']->getName())) {
+                    return null; // Skip
                 }
-                return false;
-            default:
-                return false;
+                break;
         }
+
+        return false;
     }
 
     public function getCreateTableSQL(Table $table, AbstractPlatform $platform)
     {
         $query = array();
+
         switch ($platform->getName()) {
             case 'postgresql':
                 foreach ($table->getColumns() as $column) {
-                    $query = array_merge($query, $this->getPostgresAddColumnSQL($table->getQuotedName($platform), $column->getQuotedName($platform), $column->getType()->getName()));
+                    if (!$this->isGeometryColumn($column->getType()->getName())) {
+                        continue;
+                    }
+
+                    $query = array_merge(
+                        $query,
+                        $this->getPostgresqlAddColumnSQL(
+                            $table->getQuotedName($platform),
+                            $column->getQuotedName($platform),
+                            $column->getType()->getName(),
+                            $column->getNotnull()
+                        )
+                    );
                 }
                 break;
-            default:
-                break;
         }
-        
+
         return $query;
     }
 
     public function getDropTableSQL(Table $table, AbstractPlatform $platform)
     {
         $query = array();
+
         switch ($platform->getName()) {
-            // We us DropGeometryColumn() to also drop entries from the geometry_columns table
             case 'postgresql':
                 foreach ($table->getColumns() as $column) {
-                    $query = array_merge($query, $this->getPostgresDropColumnSQL($table->getQuotedName($platform), $column->getQuotedName($platform), $column->getType()->getName()));
+                    if (!$this->isGeometryColumn($column->getType()->getName())) {
+                        continue;
+                    }
+
+                    $query = array_merge(
+                        $query,
+                        $this->getPostgresqlDropColumnSQL(
+                            $table->getQuotedName($platform),
+                            $column->getQuotedName($platform),
+                            $column->getType()->getName(),
+                            $column->getNotnull()
+                        )
+                    );
                 }
                 break;
-            default:
-                break;
         }
-        
+
         return $query;
     }
 
     public function getAddedColumnAlterTableSQL(TableDiff $diff, Column $column, AbstractPlatform $platform)
     {
-        $type = $column->getType()->getName();
-        switch (strtolower($type)) {
-            case 'point':
-            case 'linestring':
-            case 'polygon':
-            case 'multipoint':
-            case 'multilinestring':
-            case 'multipolygon':
-            case 'geometrycollection':
-                return null; // Columns will added later in getAlterTableSQL() after the table is renamed
-            default:
-                return false;
+        switch ($platform->getName()) {
+            case 'postgresql':
+                if ($this->isGeometryColumn($column->getType()->getName())) {
+                    return null; // Columns will added later in getAlterTableSQL() after the table is renamed
+                }
+                break;
         }
+
+        return false;
     }
 
     public function getRemovedColumnAlterTableSQL(TableDiff $diff, Column $column, AbstractPlatform $platform)
     {
-        return $this->getPostgresDropColumnSQL($table->getQuotedName($platform), $column->getQuotedName($platform), $column->getType()->getName());
+        switch ($platform->getName()) {
+            case 'postgresql':
+                if ($this->isGeometryColumn($column->getType()->getName())) {
+                    return $this->getPostgresqlDropColumnSQL(
+                        $diff->name,
+                        $column->getQuotedName($platform),
+                        $column->getType()->getName(),
+                        $column->getNotnull()
+                    );
+                }
+                break;
+        }
+
+        return false;
     }
 
     public function getChangedColumnAlterTableSQL(TableDiff $diff, ColumnDiff $columnDiff, AbstractPlatform $platform)
     {
         // @TODO: Check how changing is possible
 
-        // Columns can't be changed, they always have to be dropped and added
         $column = $columnDiff->column;
-        return $this->getPostgresDropColumnSQL($table->getQuotedName($platform), $column->getQuotedName($platform), $column->getType()->getName());
+
+        switch ($platform->getName()) {
+            case 'postgresql':
+                if ($this->isGeometryColumn($column->getType()->getName())) {
+                    // Columns can't be changed, they always have to be dropped and added
+                    return $this->getPostgresqlDropColumnSQL(
+                        $diff->name,
+                        $column->getQuotedName($platform),
+                        $column->getType()->getName(),
+                        $column->getNotnull()
+                    );
+                }
+                break;
+        }
+
+        return false;
     }
-    
-    public function getRenamedColumnAlterTableSQL(TableDiff $diff, $oldColumnNamem, Column $column, AbstractPlatform $platform)
+
+    public function getRenamedColumnAlterTableSQL(TableDiff $diff, $oldColumnName, Column $column, AbstractPlatform $platform)
     {
-        // Columns can't be rename, they always have to be dropped and added
-        return $this->getPostgresDropColumnSQL($table->getQuotedName($platform), $column->getQuotedName($platform), $column->getType()->getName());
+        switch ($platform->getName()) {
+            case 'postgresql':
+                if ($this->isGeometryColumn($column->getType()->getName())) {
+                    // Columns can't be renamed, they always have to be dropped and added
+                    return $this->getPostgresqlDropColumnSQL(
+                        $diff->name,
+                        $oldColumnName,
+                        $column->getType()->getName(),
+                        $column->getNotnull()
+                    );
+                }
+                break;
+        }
+
+        return false;
     }
 
     public function getAlterTableSQL(TableDiff $diff, AbstractPlatform $platform)
     {
         $query = array();
 
-        foreach ($diff->changedColumns as $oldColumnName => $column) {
-            // @TODO
+        $tableName = $diff->newName !== false ? $diff->newName : $diff->name;
+
+        foreach ($diff->addedColumns as $column) {
+            switch ($platform->getName()) {
+                case 'postgresql':
+                    if ($this->isGeometryColumn($column->getType()->getName())) {
+                        $query = array_merge(
+                            $query,
+                            $this->getPostgresqlAddColumnSQL(
+                                $tableName,
+                                $column->getQuotedName($platform),
+                                $column->getType()->getName(),
+                                $column->getNotnull()
+                            )
+                        );
+                    }
+                    break;
+            }
+        }
+
+        foreach ($diff->changedColumns as $columnDiff) {
+            switch ($platform->getName()) {
+                case 'postgresql':
+                    $column = $columnDiff->column;
+                    if ($this->isGeometryColumn($column->getType()->getName())) {
+                        $query = array_merge(
+                            $query,
+                            $this->getPostgresqlAddColumnSQL(
+                                $tableName,
+                                $column->getQuotedName($platform),
+                                $column->getType()->getName(),
+                                $column->getNotnull()
+                            )
+                        );
+                    }
+                    break;
+            }
         }
 
         foreach ($diff->renamedColumns as $oldColumnName => $column) {
-            // @TODO
+            switch ($platform->getName()) {
+                case 'postgresql':
+                    if ($this->isGeometryColumn($column->getType()->getName())) {
+                        $query = array_merge(
+                            $query,
+                            $this->getPostgresqlAddColumnSQL(
+                                $tableName,
+                                $column->getQuotedName($platform),
+                                $column->getType()->getName(),
+                                $column->getNotnull()
+                            )
+                        );
+                    }
+                    break;
+            }
         }
-        
+
         return $query;
     }
-    
-    protected function getPostgresAddColumnSQL($tableName, $columnName, $type)
+
+    protected function isGeometryColumn($type)
     {
         switch (strtolower($type)) {
             case 'point':
@@ -186,42 +322,58 @@ class SpatialSchemaHandler implements CustomSchemaHandler
             case 'multilinestring':
             case 'multipolygon':
             case 'geometrycollection':
-                // Geometry columns are created by AddGeometryColumn stored procedure
-                $query[] = sprintf(
-                    "SELECT AddGeometryColumn('%s', '%s', %d, '%s', %d)",
-                    $tableName, // Table name
-                    $columnName, // Column name
-                    -1, // SRID
-                    strtoupper($type), // Geometry type
-                    2 // Dimension
-                );
-
-                if ($column->getNotnull()) {
-                    // Add a NOT NULL constraint to the field
-                    $query[] = sprintf(
-                        "ALTER TABLE %s ALTER %s SET NOT NULL",
-                        $tableName, // Table name
-                        $columnName // Column name
-                    );
-                }
+                return true;
+            default:
+                return false;
         }
     }
-    
-    protected function getPostgresDropColumnSQL($tableName, $columnName, $type)
+
+    protected function getPostgresqlAddColumnSQL($tableName, $columnName, $type, $notnull)
     {
-        switch (strtolower($type)) {
-            case 'point':
-            case 'linestring':
-            case 'polygon':
-            case 'multipoint':
-            case 'multilinestring':
-            case 'multipolygon':
-            case 'geometrycollection':
-                $query[] = sprintf(
-                    "SELECT DropGeometryColumn ('%s', '%s')",
-                    $tableName, // Table name
-                    $columnName // Column name
-                );
+        $query = array();
+
+        // Geometry columns are created by AddGeometryColumn stored procedure
+        $query[] = sprintf(
+            "SELECT AddGeometryColumn('%s', '%s', %d, '%s', %d)",
+            $tableName, // Table name
+            $columnName, // Column name
+            -1, // SRID
+            strtoupper($type), // Geometry type
+            2 // Dimension
+        );
+
+        if ($notnull) {
+            // Add a NOT NULL constraint to the field
+            $query[] = sprintf(
+                "ALTER TABLE %s ALTER %s SET NOT NULL",
+                $tableName, // Table name
+                $columnName // Column name
+            );
         }
+
+        return $query;
+    }
+
+    protected function getPostgresqlDropColumnSQL($tableName, $columnName, $type, $notnull)
+    {
+        $query = array();
+        
+        if ($notnull) {
+            // Remove NOT NULL constraint from the field
+            $query[] = sprintf(
+                "ALTER TABLE %s ALTER %s SET DEFAULT NULL",
+                $tableName, // Table name
+                $columnName // Column name
+            );
+        }
+
+        // We use DropGeometryColumn() to also drop entries from the geometry_columns table
+        $query[] = sprintf(
+            "SELECT DropGeometryColumn ('%s', '%s')",
+            $tableName, // Table name
+            $columnName // Column name
+        );
+
+        return $query;
     }
 }
